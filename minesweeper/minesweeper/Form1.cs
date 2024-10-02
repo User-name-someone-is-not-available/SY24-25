976f2c1467b80cefea0a91420359b654ba9d460e
I'manidiot <rzsto600@learner.dvusd.org>
2024-10-02 09:52:15 -0700

    a lot of everything
    been a bit hasn't it

--- minesweeper/minesweeper/Form1.cs
+++ minesweeper/minesweeper/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,10 @@ using System.Windows.Forms;
 
 namespace minesweeper
 {
+
     public partial class Form1 : Form
     {
+        Random Random = new Random();
         Button[] btnGrid = new Button[100];
         tile[] tileGrid = new tile[100];
         public Form1()
@@ -21,7 +24,8 @@ namespace minesweeper
         }
         private Button GetButton(int R, int C) 
         {
-            return (Button)GetButton(R, C);
+            int idx = (R - 1) * 10 + (C - 1);
+            return btnGrid[idx];
         }
         private int getIndex(Button b)
         {
@@ -30,20 +34,208 @@ namespace minesweeper
             int.TryParse(tmp, out retVal);
             return retVal-1; 
         }
+        private void Setcounts()
+        {
+            //for everytile on the board,add 1 for every ajecent mine, set that count in the tile 
+            for (int r=1; r<11; r++)
+            {
+                for (int c=1; c<11; c++)
+                {
+                    GetButton(r, c).BackColor = Color.AliceBlue;
+
+                }
+
+            }
+        }
+        private int countadj(int r, int c)
+        {
+            int minecount = 0;
+            if (r > 1 && c > 1)
+            {
+              if(tileGrid[getIndex(GetButton(r - 1, c - 1))].getmine()) { minecount ++; }
+            }
+            if (r > 1 )
+            {
+                if (tileGrid[getIndex(GetButton(r - 1, c))].getmine()) { minecount++; }
+            }
+            if (r > 1 && c < 10)
+            {
+                if (tileGrid[getIndex(GetButton(r - 1, c + 1))].getmine()) { minecount++; }
+            }
+            if (c > 1)
+            {
+                if (tileGrid[getIndex(GetButton(r, c - 1))].getmine()) { minecount++; }
+            } 
+            if (c < 10) 
+            {
+                if (tileGrid[getIndex(GetButton(r, c + 1))].getmine()) { minecount++; }
+            }
+            if (r < 10 && c > 1) 
+            {
+                if (tileGrid[getIndex(GetButton(r + 1, c - 1))].getmine()) { minecount++; }
+            }
+
+            if (r < 10)
+            {
+                if (tileGrid[getIndex(GetButton(r + 1, c))].getmine()) { minecount++; }
+            }
+            if (r < 10 && c < 10)
+            {
+                if (tileGrid[getIndex(GetButton(r + 1, c + 1))].getmine()) { minecount++; }
+            }
+            return minecount;
+        }
+        private int countadjFlag(int r, int c)
+        {
+            int flagcount = 0;
+            if (r > 1 && c > 1)
+            {
+                if (tileGrid[getIndex(GetButton(r - 1, c - 1))].getflag()) { flagcount++; }
+            }
+            if (r > 1)
+            {
+                if (tileGrid[getIndex(GetButton(r - 1, c))].getflag()) { flagcount++; }
+            }
+            if (r > 1 && c < 10)
+            {
+                if (tileGrid[getIndex(GetButton(r - 1, c + 1))].getflag()) { flagcount++; }
+            }
+            if (c > 1)
+            {
+                if (tileGrid[getIndex(GetButton(r, c - 1))].getflag()) { flagcount++; }
+            }
+            if (c < 10)
+            {
+                if (tileGrid[getIndex(GetButton(r, c + 1))].getflag()) { flagcount++; }
+            }
+            if (r < 10 && c > 1)
+            {
+                if (tileGrid[getIndex(GetButton(r + 1, c - 1))].getflag()) { flagcount++; }
+            }
+
+            if (r < 10)
+            {
+                if (tileGrid[getIndex(GetButton(r + 1, c))].getflag()) { flagcount++; }
+            }
+            if (r < 10 && c < 10)
+            {
+                if (tileGrid[getIndex(GetButton(r + 1, c + 1))].getflag()) { flagcount++; }
+            }
+            return flagcount;
+        }
+        private void dignear(int r, int c)
+        {
+            
+            if (r > 1 && c > 1)
+            {
+                tileGrid[getIndex(GetButton(r - 1, c - 1))].setdug();
+            }
+            if (r > 1)
+            {
+                tileGrid[getIndex(GetButton(r - 1, c))].setdug();
+            }
+            if (r > 1 && c < 10)
+            {
+                tileGrid[getIndex(GetButton(r - 1, c + 1))].setdug();
+            }
+            if (c > 1)
+            {
+                tileGrid[getIndex(GetButton(r, c - 1))].setdug();
+            }
+            if (c < 10)
+            {
+                tileGrid[getIndex(GetButton(r, c + 1))].setdug();
+            }
+            if (r < 10 && c > 1)
+            {
+                tileGrid[getIndex(GetButton(r + 1, c - 1))].setdug();
+            }
+
+            if (r < 10)
+            {
+               tileGrid[getIndex(GetButton(r + 1, c))].setdug();
+            }
+            if (r < 10 && c < 10)
+            {
+                tileGrid[getIndex(GetButton(r + 1, c + 1))].setdug();
+            }
+           
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        private void button91_MouseDown(object sender, MouseEventArgs e)
+        //    for (int r = 1; r< 11; r++)
+        //        {
+        //          for (int c = 1; c< 11; c++)
+        //             {
+        //                  tileGrid[getIndex(GetButton(r, c))].setnearby(countadj(r, c));
+        // tileGrid[getIndex(GetButton(r, c))].setnearbyFlags(countadjFlag(r, c));
+
+        // }
+        private void reveal() {
+            for (int r = 1; r < 11; r++)
+            {
+                for (int c = 1; c < 11; c++)
+                {
+                    tileGrid[getIndex(GetButton(r, c))].setnearby(countadj(r, c));
+                    tileGrid[getIndex(GetButton(r, c))].setnearbyFlags(countadjFlag(r, c));
+                }
+            }
+        }
+        private void Middleclick(object sender, MouseEventArgs e, int r, int c )
+        {
+            
+            Button b = sender as Button;
+            tile T = tileGrid[getIndex(b)];
+            if (e.Button == MouseButtons.Middle )
+            {
+                if (countadj(r,c ) == countadjFlag(r, c)) 
+                { 
+                    dignear(r, c);
+                }
+                                
+                
+
+                
+            }
+        }
+        
+        private void button_MouseDown(object sender, MouseEventArgs e)
         {
             Button b = sender as Button;
-            b.BackColor = Color.Red;
+            tile T = tileGrid[getIndex(b)];
+
+
+            if (e.Button == MouseButtons.Right)
+            {
+
+                T.setflag();
+            }
+
+            else if (e.Button == MouseButtons.Left)
+                T.setdug();
         }
         private void button101_Click(object sender, EventArgs e)
         {
             reset();
         }
+        private void createmines(int nubmines)
+        {
+            int minecount = 0;
+            //    untill we have enough mines]
+            while (minecount < nubmines)
+            {
+                //generate nuber 1-100    set mine on that tile if it doesnt have a mine
+                int rNum = Random.Next(0, 100);
+                if(tileGrid[rNum].getmine() == false)
+                {
+                    minecount++;
+                    tileGrid[rNum].setmine(true); 
+                }
 
+            }
+        }
         private void reset()
         {
             reset();
@@ -52,11 +244,33 @@ namespace minesweeper
             {
                 for (int i = 0; i < 100; i++)
                 {
+                    
                     btnGrid[i] = (Button)Controls["Button" + (i + 1)];
-                    btnGrid[i].BackColor = Color.Gray;
                     tileGrid[i] = new tile(btnGrid[i]);
+                    tileGrid[i].setflagimage(flagpicturebox.Image);
+                    tileGrid[i].setmineimage(minePIcturebox.Image);
+                    
+
                 }
+                createmines(30);
+                //Setcounts();
+                // countadj(10,10);
+                // countadj(1, 10);
+                // countadj(1, 1);
+                // countadj(10, 1);
+                // countadj(5, 5);
+                for (int r = 1; r<11; r++)
+                {
+                    for( int c=1; c < 11; c++)
+                    {
+                        tileGrid[getIndex(GetButton(r, c))].setnearby(countadj(r,c));
+                    }
+                }
+              
             }
+            
         }
+
+     
     }
 }
--- minesweeper/minesweeper/tile.cs
+++ minesweeper/minesweeper/tile.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -9,13 +11,61 @@ namespace minesweeper
 {
     internal class tile
     {
-        Button m_b;
-        Boolean m_flag;
-        public tile(Button b) 
+        private Button T_b;
+        private Boolean T_flag;
+        private Boolean T_dug;
+        private Boolean T_mine;
+        int T_nearmines;
+        private Image T_flagimage;
+        private Image T_mineimage;
+        int T_nearflags;
+        public tile(Button b)
         {
-            m_b = b;
+
+            T_b = b;
+            T_b.BackColor = Color.Gray;
         }
+        
+        public void setnearby(int N) 
+        {
+            
+            T_nearmines = N;
+            T_b.Text = T_nearmines.ToString();
+        }
+        public void setnearbyFlags(int N)
+        {
 
+            T_nearflags = N;
+            T_b.Text = T_nearflags.ToString();
+        }
+        public void setflagimage(Image flagimage) { T_flagimage = flagimage; }
+        public void setmineimage(Image mineimage) { T_mineimage = mineimage; }
+        public void setdug() 
+        {
+            T_dug = true;
+            if (T_mine)
+                T_b.BackgroundImage = T_mineimage;
+            else
+                T_b.BackColor = Color.Beige;
+            
 
+        }
+        public Boolean getmine() { return T_mine; }
+        public Boolean getflag() { return T_flag; }
+        public void setmine(Boolean b) 
+        {
+            T_mine = b;
+          
+        }
+        public void setflag()
+        {
+            
+            T_flag = !T_flag;
+            if (T_flag)
+                T_b.BackgroundImage = T_flagimage;
+            
+            else
+                T_b.BackgroundImage = null;
+        }
     }
 }
