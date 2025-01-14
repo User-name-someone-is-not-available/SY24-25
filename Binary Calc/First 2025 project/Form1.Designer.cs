cc67f9cd39b3c8b179fd63f098fc203e9dc699f3
I'manidiot <rzsto600@learner.dvusd.org>
2025-01-14 09:27:03 -0700

    I didn't Miss This
    9

--- Binary Calc/First 2025 project/Form1.Designer.cs
+++ Binary Calc/First 2025 project/Form1.Designer.cs
@@ -37,12 +37,29 @@
             this.Bit8 = new System.Windows.Forms.TextBox();
             this.Bit7 = new System.Windows.Forms.TextBox();
             this.Bit5 = new System.Windows.Forms.TextBox();
+            this.BitB6 = new System.Windows.Forms.TextBox();
+            this.BitB8 = new System.Windows.Forms.TextBox();
+            this.BitB7 = new System.Windows.Forms.TextBox();
+            this.BitB5 = new System.Windows.Forms.TextBox();
+            this.BitB2 = new System.Windows.Forms.TextBox();
+            this.BitB4 = new System.Windows.Forms.TextBox();
+            this.BitB3 = new System.Windows.Forms.TextBox();
+            this.BitB1 = new System.Windows.Forms.TextBox();
+            this.BitC6 = new System.Windows.Forms.TextBox();
+            this.BitC8 = new System.Windows.Forms.TextBox();
+            this.BitC7 = new System.Windows.Forms.TextBox();
+            this.BitC5 = new System.Windows.Forms.TextBox();
+            this.BitC2 = new System.Windows.Forms.TextBox();
+            this.BitC4 = new System.Windows.Forms.TextBox();
+            this.BitC3 = new System.Windows.Forms.TextBox();
+            this.BitC1 = new System.Windows.Forms.TextBox();
+            this.TotallabelB = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Bit1
             // 
             this.Bit1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit1.Location = new System.Drawing.Point(12, 169);
+            this.Bit1.Location = new System.Drawing.Point(-1, 325);
             this.Bit1.MaxLength = 1;
             this.Bit1.Multiline = true;
             this.Bit1.Name = "Bit1";
@@ -54,7 +71,7 @@
             // Bit3
             // 
             this.Bit3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit3.Location = new System.Drawing.Point(198, 169);
+            this.Bit3.Location = new System.Drawing.Point(185, 325);
             this.Bit3.MaxLength = 1;
             this.Bit3.Multiline = true;
             this.Bit3.Name = "Bit3";
@@ -66,7 +83,7 @@
             // Bit4
             // 
             this.Bit4.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit4.Location = new System.Drawing.Point(291, 169);
+            this.Bit4.Location = new System.Drawing.Point(278, 325);
             this.Bit4.MaxLength = 1;
             this.Bit4.Multiline = true;
             this.Bit4.Name = "Bit4";
@@ -78,7 +95,7 @@
             // Bit2
             // 
             this.Bit2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit2.Location = new System.Drawing.Point(105, 169);
+            this.Bit2.Location = new System.Drawing.Point(92, 325);
             this.Bit2.MaxLength = 1;
             this.Bit2.Multiline = true;
             this.Bit2.Name = "Bit2";
@@ -90,7 +107,7 @@
             // totalLabel
             // 
             this.totalLabel.AutoSize = true;
-            this.totalLabel.Location = new System.Drawing.Point(368, 82);
+            this.totalLabel.Location = new System.Drawing.Point(744, 359);
             this.totalLabel.Name = "totalLabel";
             this.totalLabel.Size = new System.Drawing.Size(44, 16);
             this.totalLabel.TabIndex = 5;
@@ -99,7 +116,7 @@
             // Bit6
             // 
             this.Bit6.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit6.Location = new System.Drawing.Point(478, 169);
+            this.Bit6.Location = new System.Drawing.Point(465, 325);
             this.Bit6.MaxLength = 1;
             this.Bit6.Multiline = true;
             this.Bit6.Name = "Bit6";
@@ -111,7 +128,7 @@
             // Bit8
             // 
             this.Bit8.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit8.Location = new System.Drawing.Point(664, 169);
+            this.Bit8.Location = new System.Drawing.Point(651, 325);
             this.Bit8.MaxLength = 1;
             this.Bit8.Multiline = true;
             this.Bit8.Name = "Bit8";
@@ -123,7 +140,7 @@
             // Bit7
             // 
             this.Bit7.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit7.Location = new System.Drawing.Point(571, 169);
+            this.Bit7.Location = new System.Drawing.Point(558, 325);
             this.Bit7.MaxLength = 1;
             this.Bit7.Multiline = true;
             this.Bit7.Name = "Bit7";
@@ -135,7 +152,7 @@
             // Bit5
             // 
             this.Bit5.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bit5.Location = new System.Drawing.Point(385, 169);
+            this.Bit5.Location = new System.Drawing.Point(372, 325);
             this.Bit5.MaxLength = 1;
             this.Bit5.Multiline = true;
             this.Bit5.Name = "Bit5";
@@ -144,11 +161,229 @@
             this.Bit5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.Bit5.TextChanged += new System.EventHandler(this.Bit6_TextChanged);
             // 
+            // BitB6
+            // 
+            this.BitB6.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitB6.Location = new System.Drawing.Point(465, 207);
+            this.BitB6.MaxLength = 1;
+            this.BitB6.Multiline = true;
+            this.BitB6.Name = "BitB6";
+            this.BitB6.Size = new System.Drawing.Size(87, 84);
+            this.BitB6.TabIndex = 17;
+            this.BitB6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BitB6.TextChanged += new System.EventHandler(this.BitB1_TextChanged);
+            // 
+            // BitB8
+            // 
+            this.BitB8.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitB8.Location = new System.Drawing.Point(651, 207);
+            this.BitB8.MaxLength = 1;
+            this.BitB8.Multiline = true;
+            this.BitB8.Name = "BitB8";
+            this.BitB8.Size = new System.Drawing.Size(87, 84);
+            this.BitB8.TabIndex = 16;
+            this.BitB8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BitB8.TextChanged += new System.EventHandler(this.BitB1_TextChanged);
+            // 
+            // BitB7
+            // 
+            this.BitB7.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitB7.Location = new System.Drawing.Point(558, 207);
+            this.BitB7.MaxLength = 1;
+            this.BitB7.Multiline = true;
+            this.BitB7.Name = "BitB7";
+            this.BitB7.Size = new System.Drawing.Size(87, 84);
+            this.BitB7.TabIndex = 15;
+            this.BitB7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BitB7.TextChanged += new System.EventHandler(this.BitB1_TextChanged);
+            // 
+            // BitB5
+            // 
+            this.BitB5.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitB5.Location = new System.Drawing.Point(372, 207);
+            this.BitB5.MaxLength = 1;
+            this.BitB5.Multiline = true;
+            this.BitB5.Name = "BitB5";
+            this.BitB5.Size = new System.Drawing.Size(87, 84);
+            this.BitB5.TabIndex = 14;
+            this.BitB5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BitB5.TextChanged += new System.EventHandler(this.BitB1_TextChanged);
+            // 
+            // BitB2
+            // 
+            this.BitB2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitB2.Location = new System.Drawing.Point(92, 207);
+            this.BitB2.MaxLength = 1;
+            this.BitB2.Multiline = true;
+            this.BitB2.Name = "BitB2";
+            this.BitB2.Size = new System.Drawing.Size(87, 84);
+            this.BitB2.TabIndex = 13;
+            this.BitB2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BitB2.TextChanged += new System.EventHandler(this.BitB1_TextChanged);
+            // 
+            // BitB4
+            // 
+            this.BitB4.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitB4.Location = new System.Drawing.Point(278, 207);
+            this.BitB4.MaxLength = 1;
+            this.BitB4.Multiline = true;
+            this.BitB4.Name = "BitB4";
+            this.BitB4.Size = new System.Drawing.Size(87, 84);
+            this.BitB4.TabIndex = 12;
+            this.BitB4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BitB4.TextChanged += new System.EventHandler(this.BitB1_TextChanged);
+            // 
+            // BitB3
+            // 
+            this.BitB3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitB3.Location = new System.Drawing.Point(185, 207);
+            this.BitB3.MaxLength = 1;
+            this.BitB3.Multiline = true;
+            this.BitB3.Name = "BitB3";
+            this.BitB3.Size = new System.Drawing.Size(87, 84);
+            this.BitB3.TabIndex = 11;
+            this.BitB3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BitB3.TextChanged += new System.EventHandler(this.BitB1_TextChanged);
+            // 
+            // BitB1
+            // 
+            this.BitB1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitB1.Location = new System.Drawing.Point(-1, 207);
+            this.BitB1.MaxLength = 1;
+            this.BitB1.Multiline = true;
+            this.BitB1.Name = "BitB1";
+            this.BitB1.Size = new System.Drawing.Size(87, 84);
+            this.BitB1.TabIndex = 10;
+            this.BitB1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BitB1.TextChanged += new System.EventHandler(this.BitB1_TextChanged);
+            // 
+            // BitC6
+            // 
+            this.BitC6.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitC6.Location = new System.Drawing.Point(465, 101);
+            this.BitC6.MaxLength = 1;
+            this.BitC6.Multiline = true;
+            this.BitC6.Name = "BitC6";
+            this.BitC6.Size = new System.Drawing.Size(87, 84);
+            this.BitC6.TabIndex = 25;
+            this.BitC6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            
+            // 
+            // BitC8
+            // 
+            this.BitC8.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitC8.Location = new System.Drawing.Point(651, 101);
+            this.BitC8.MaxLength = 1;
+            this.BitC8.Multiline = true;
+            this.BitC8.Name = "BitC8";
+            this.BitC8.Size = new System.Drawing.Size(87, 84);
+            this.BitC8.TabIndex = 24;
+            this.BitC8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+         
+            // 
+            // BitC7
+            // 
+            this.BitC7.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitC7.Location = new System.Drawing.Point(558, 101);
+            this.BitC7.MaxLength = 1;
+            this.BitC7.Multiline = true;
+            this.BitC7.Name = "BitC7";
+            this.BitC7.Size = new System.Drawing.Size(87, 84);
+            this.BitC7.TabIndex = 23;
+            this.BitC7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+       
+            // 
+            // BitC5
+            // 
+            this.BitC5.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitC5.Location = new System.Drawing.Point(372, 101);
+            this.BitC5.MaxLength = 1;
+            this.BitC5.Multiline = true;
+            this.BitC5.Name = "BitC5";
+            this.BitC5.Size = new System.Drawing.Size(87, 84);
+            this.BitC5.TabIndex = 22;
+            this.BitC5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+           
+            // 
+            // BitC2
+            // 
+            this.BitC2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitC2.Location = new System.Drawing.Point(92, 101);
+            this.BitC2.MaxLength = 1;
+            this.BitC2.Multiline = true;
+            this.BitC2.Name = "BitC2";
+            this.BitC2.Size = new System.Drawing.Size(87, 84);
+            this.BitC2.TabIndex = 21;
+            this.BitC2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+       
+            // 
+            // BitC4
+            // 
+            this.BitC4.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitC4.Location = new System.Drawing.Point(278, 101);
+            this.BitC4.MaxLength = 1;
+            this.BitC4.Multiline = true;
+            this.BitC4.Name = "BitC4";
+            this.BitC4.Size = new System.Drawing.Size(87, 84);
+            this.BitC4.TabIndex = 20;
+            this.BitC4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+           
+            // 
+            // BitC3
+            // 
+            this.BitC3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitC3.Location = new System.Drawing.Point(185, 101);
+            this.BitC3.MaxLength = 1;
+            this.BitC3.Multiline = true;
+            this.BitC3.Name = "BitC3";
+            this.BitC3.Size = new System.Drawing.Size(87, 84);
+            this.BitC3.TabIndex = 19;
+            this.BitC3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+        
+            // 
+            // BitC1
+            // 
+            this.BitC1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitC1.Location = new System.Drawing.Point(-1, 101);
+            this.BitC1.MaxLength = 1;
+            this.BitC1.Multiline = true;
+            this.BitC1.Name = "BitC1";
+            this.BitC1.Size = new System.Drawing.Size(87, 84);
+            this.BitC1.TabIndex = 18;
+            this.BitC1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+         
+            // 
+            // TotallabelB
+            // 
+            this.TotallabelB.AutoSize = true;
+            this.TotallabelB.Location = new System.Drawing.Point(744, 244);
+            this.TotallabelB.Name = "TotallabelB";
+            this.TotallabelB.Size = new System.Drawing.Size(44, 16);
+            this.TotallabelB.TabIndex = 26;
+            this.TotallabelB.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TotallabelB);
+            this.Controls.Add(this.BitC6);
+            this.Controls.Add(this.BitC8);
+            this.Controls.Add(this.BitC7);
+            this.Controls.Add(this.BitC5);
+            this.Controls.Add(this.BitC2);
+            this.Controls.Add(this.BitC4);
+            this.Controls.Add(this.BitC3);
+            this.Controls.Add(this.BitC1);
+            this.Controls.Add(this.BitB6);
+            this.Controls.Add(this.BitB8);
+            this.Controls.Add(this.BitB7);
+            this.Controls.Add(this.BitB5);
+            this.Controls.Add(this.BitB2);
+            this.Controls.Add(this.BitB4);
+            this.Controls.Add(this.BitB3);
+            this.Controls.Add(this.BitB1);
             this.Controls.Add(this.Bit6);
             this.Controls.Add(this.Bit8);
             this.Controls.Add(this.Bit7);
@@ -176,6 +411,23 @@
         private System.Windows.Forms.TextBox Bit8;
         private System.Windows.Forms.TextBox Bit7;
         private System.Windows.Forms.TextBox Bit5;
+        private System.Windows.Forms.TextBox BitB6;
+        private System.Windows.Forms.TextBox BitB8;
+        private System.Windows.Forms.TextBox BitB7;
+        private System.Windows.Forms.TextBox BitB5;
+        private System.Windows.Forms.TextBox BitB2;
+        private System.Windows.Forms.TextBox BitB4;
+        private System.Windows.Forms.TextBox BitB3;
+        private System.Windows.Forms.TextBox BitB1;
+        private System.Windows.Forms.TextBox BitC6;
+        private System.Windows.Forms.TextBox BitC8;
+        private System.Windows.Forms.TextBox BitC7;
+        private System.Windows.Forms.TextBox BitC5;
+        private System.Windows.Forms.TextBox BitC2;
+        private System.Windows.Forms.TextBox BitC4;
+        private System.Windows.Forms.TextBox BitC3;
+        private System.Windows.Forms.TextBox BitC1;
+        private System.Windows.Forms.Label TotallabelB;
     }
 }
 
--- Binary Calc/First 2025 project/Form1.cs
+++ Binary Calc/First 2025 project/Form1.cs
@@ -16,6 +16,7 @@ namespace First_2025_project
     {
         int Value = 0;
         int[] bits = new int[8];
+        int[] bitsB = new int[8];
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +24,6 @@ namespace First_2025_project
         private void calculateValue()
         {
             Value = 0;
-
             if (bits[7] != 0) Value += 1;
             if (bits[6] != 0) Value += 2;
             if (bits[5] != 0) Value += 4;
@@ -32,10 +32,9 @@ namespace First_2025_project
             if (bits[2] != 0) Value += 32;
             if (bits[1] != 0) Value += 64;
             if (bits[0] != 0) Value += 128;
-
             totalLabel.Text = Value.ToString();
         }
-
+        //The Change We Made 
         private void Bit6_TextChanged(object sender, EventArgs e)
         {
             if (Bit8.Text == "1") bits[7] = 1;
@@ -64,6 +63,48 @@ namespace First_2025_project
             //bit8
 
             calculateValue();
+        }
+
+       
+        private void calculateValueB()
+        {
+            Value = 0;
+            if (bits[7] != 0) Value += 1;
+            if (bits[6] != 0) Value += 2;
+            if (bits[5] != 0) Value += 4;
+            if (bits[4] != 0) Value += 8;
+            if (bits[3] != 0) Value += 16;
+            if (bits[2] != 0) Value += 32;
+            if (bits[1] != 0) Value += 64;
+            if (bits[0] != 0) Value += 128;
+            TotallabelB.Text = Value.ToString();
+        }
+        private void BitB1_TextChanged(object sender, EventArgs e)
+        {
+            if (BitB8.Text == "1") bits[7] = 1;
+            else bits[7] = 0;
+            //bit1
+            if (BitB7.Text == "1") bits[6] = 1;
+            else bits[6] = 0;
+            //bit2
+            if (BitB6.Text == "1") bits[5] = 1;
+            else bits[5] = 0;
+            //bit3
+            if (BitB5.Text == "1") bits[4] = 1;
+            else bits[4] = 0;
+            //bit4
+            if (BitB4.Text == "1") bits[3] = 1;
+            else bits[3] = 0;
+            //bit3
+            if (BitB3.Text == "1") bits[2] = 1;
+            else bits[2] = 0;
+            //bit6
+            if (BitB2.Text == "1") bits[1] = 1;
+            else bits[1] = 0;
+            //bit7
+            if (BitB1.Text == "1") bits[0] = 1;
+            else bits[0] = 0;
+            calculateValueB();
         }
     }
 }
